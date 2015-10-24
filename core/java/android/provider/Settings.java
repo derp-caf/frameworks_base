d1e012b6dd31a51bac84d6b7532bb748f08d3799
beanstown106 <nbenis106@gmail.com>
2015-10-23 21:07:35 -0400

    SystemUI: Allow to enable Lockscreen Media Art [1/2]
    arrow-11.0 edits:
     - Adapt to observer
    
    Change-Id: Id93e115649df5d6dc680101a8c852644b217c28a
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5236,6 +5236,12 @@ public final class Settings {
         public static final String QS_DATAUSAGE = "qs_datausage";
 
         /**
+         * Lockscreen media art
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5373,6 +5379,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(LOCKSCREEN_PIN_SCRAMBLE_LAYOUT);
             PRIVATE_SETTINGS.add(VOLUME_KEY_CURSOR_CONTROL);
             PRIVATE_SETTINGS.add(QS_DATAUSAGE);
+            PRIVATE_SETTINGS.add(LOCKSCREEN_MEDIA_METADATA);
         }
 
         /**
