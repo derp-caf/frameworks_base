987c78792f27fc2cbeb5fec082e67a84645c31ca
bigrushdog <randall.rushing@gmail.com>
2017-09-06 20:46:51 +0200

    core: Add api to take screenshots
    Omni: extracted from DUI
    add a public call to XtendedUtils
    
    Change-Id: If71944466c703a9b3fe54aea6bb21150b0a082bb
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/com/android/internal/util/derpcaf/DerpcafUtils.java
+++ core/java/com/android/internal/util/derpcaf/DerpcafUtils.java
@@ -55,6 +55,9 @@ import com.android.internal.statusbar.IStatusBarService;
  */
 public class DerpcafUtils {
 
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
+
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
         if (pm!= null) {
@@ -202,6 +205,14 @@ public class DerpcafUtils {
         context.sendBroadcastAsUser(keyguardIntent, user);
     }
 
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
