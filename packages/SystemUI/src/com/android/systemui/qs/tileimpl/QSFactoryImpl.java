5bec56055af62f861b57cd8737d4ace1bbe2d672
mydongistiny <jaysonedson@gmail.com>
2018-06-17 17:44:52 -0700

    QS: SystemUI: Add On-The-Go Tile
    Change-Id: Ic345f83723a48e3a7d610926fb9b47fab7ef878f
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.OnTheGoTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -170,6 +171,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mNfcTileProvider.get();
             case "dark":
                 return mUiModeNightTileProvider.get();
+            case "onthego":
+                return new OnTheGoTile(mHost);
         }
 
         // Intent tiles.
