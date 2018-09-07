292785ad00ac9326066e37aceef9921058d1587b
maxwen <max.weninger@gmail.com>
2018-09-07 11:47:40 +0200

    Partial Revert: "base: SystemUI: keyguard weather view - fix initial kick off after boot"
    Change-Id: I64c5082ed675549d67728bcd7a8e19c09c7a21e7

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -119,7 +119,6 @@ public class KeyguardStatusView extends GridLayout implements
             refreshFormat();
             updateOwnerInfo();
             updateLogoutView();
-            updateSettings();
             refreshLockFont();
         }
 
@@ -213,7 +212,7 @@ public class KeyguardStatusView extends GridLayout implements
         updateOwnerInfo();
         updateLogoutView();
         updateDark();
-        updateSettings();
+	updateSettings();
         refreshLockFont();
 
         // Disable elegant text height because our fancy colon makes the ymin value huge for no
