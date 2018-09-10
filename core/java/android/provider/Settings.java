129d24fee3e6043ff79c34b37dca504519c6379c
ezio84 <brabus84@gmail.com>
2018-09-10 18:06:52 +0530

    BatteryMeterDrawable: add big-circle & big dotted circle battery styles [1/2] + Some Improvement in BatteryMeterView

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8548,8 +8548,10 @@ public final class Settings {
          * 0: Display the battery an icon in portrait mode
          * 1: Display the battery as a circle
          * 2: Display the battery as a dotted circle
-         * 3: Display the battery as text
-         * 4: Do not display the battery
+	 * 3: Display the battery as big circle
+	 * 4: Display the battery as big dotted circle
+         * 5: Display the battery as text
+         * 6: Do not display the battery
          * default: 0
          * @hide
          */
@@ -8557,7 +8559,7 @@ public final class Settings {
 
         /** @hide */
         public static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 4);
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 6);
 
 	/**
          * Enable face auto unlock on secure lock screens
