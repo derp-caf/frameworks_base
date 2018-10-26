9702483c3adf8604d507e0c7f5aa177365cb20ec
maxwen <max.weninger@gmail.com>
2018-10-26 21:48:03 +0200

    base: fix check for keyguard in edge gestures
    disable only if keyguard is really shown and in front
    
    Change-Id: I232c68515a50dd6b87e9929b0d1a81e725d8e314

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -3077,6 +3077,7 @@ public class WindowManagerService extends IWindowManager.Stub
         return mPolicy.isKeyguardLocked();
     }
 
+    @Override
     public boolean isKeyguardShowingAndNotOccluded() {
         return mPolicy.isKeyguardShowingAndNotOccluded();
     }
