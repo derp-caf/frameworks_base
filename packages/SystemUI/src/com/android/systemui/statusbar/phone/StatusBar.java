65d64cfc4d828d39b9895cb55e258fbd52c61693
bigrushdog <randall.rushing@gmail.com>
2018-11-01 14:28:54 +0100

    Navbar Pulse bringup
    Change-Id: I9428fbd16c4e51c4778b0e304f74e62b9efaee5e
    ezio84: refactor to work with stock P navbar

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1247,6 +1247,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     }
 
+    public NotificationMediaManager getMediaManager() {
+        return mMediaManager;
+    }
+
     /**
      * Returns the {@link android.view.View.OnTouchListener} that will be invoked when the
      * background window of the status bar is clicked.
