72612e5cdd36b0b882bc0903112394574e1bb52f
ezio84 <brabus84@gmail.com>
2018-11-01 11:20:55 +0100

    Full nav gesture: home action on the whole bar and doubletap2sleep [1/2]
    all this stuff comes from what we did on dui (waiting for it on P!)
    so huge thanks to @bigrushdog
    
    Change-Id: I5b4fd0ba7597f121fe44a920024f42931dd139fd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5668,6 +5668,11 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+	 * @hide
+	 */
+        public static final String FULL_GESTURE_NAVBAR_DT2S = "full_gesture_navbar_dt2s";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
