24b86903b45a355b78ed90db55e7287eb0411635
SpiritCroc <dev@spiritcroc.de>
2018-11-11 13:01:56 +0000

    Add network traffic monitor properly to statusbar icons
    Make it behave fine on devices that don't have much status bar space
    (e.g. because of a display cutout/"notch")
    
    Arrow edits:- nuke from status_bar.xml
    
    Change-Id: Icbb2715ae78ce06bd3b79dc23ff491db202985f2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mBattery, mClock, mLogoIcon, mLogoIconRight, mCenterClock, mRightClock, mNetworkTraffic;
+    private View mLeftSide, mStatusIcons, mBattery, mClock, mLogoIcon, mLogoIconRight, mCenterClock, mRightClock;
 
     private Animator mCurrentAnimation;
 
@@ -52,7 +52,6 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mRightClock = mView.findViewById(R.id.right_clock);
 	mLogoIcon = mView.findViewById(R.id.status_bar_logo);
 	mLogoIconRight = mView.findViewById(R.id.status_bar_logo_right);
-	mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -100,7 +99,6 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mRightClock, newAlphaBC),
 		    animateTransitionTo(mLogoIcon, newAlpha),
 		    animateTransitionTo(mLogoIconRight, newAlpha)
-		    animateTransitionTo(mNetworkTraffic, newAlpha)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -117,7 +115,6 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mRightClock.setAlpha(newAlphaBC);
 	    mLogoIcon.setAlpha(newAlpha);
 	    mLogoIconRight.setAlpha(newAlpha);
-	    mNetworkTraffic.setAlpha(newAlpha);
         }
     }
 }
