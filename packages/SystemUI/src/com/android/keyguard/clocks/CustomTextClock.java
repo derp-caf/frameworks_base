4d2fdad2e558abdc3496dd575b0c81940feab418
prometheusXderpgang <prometheusXderpgang@gmail.com>
2019-05-05 15:14:40 +0530

    Revert "CustomTextClock : User Selective Size & Color for Q Clock [1/2]"
    This reverts commit 53f066aa57cdf862344b4eb2f4679809681675eb.

--- packages/SystemUI/src/com/android/keyguard/clocks/CustomTextClock.java
+++ packages/SystemUI/src/com/android/keyguard/clocks/CustomTextClock.java
@@ -25,11 +25,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.database.ContentObserver;
 import android.graphics.Canvas;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.UserHandle;
 import android.text.format.DateUtils;
 import android.text.format.DateFormat;
 import android.text.format.Time;
@@ -80,10 +76,6 @@ public class CustomTextClock extends TextView {
     private Context mContext;
 
     private boolean h24;
-
-    private int mClockColor = 0xffffffff;
-    private int mClockSize = 32;
-    private SettingsObserver mSettingsObserver;
 
     public CustomTextClock(Context context) {
         this(context, null);
@@ -136,13 +128,6 @@ public class CustomTextClock extends TextView {
 
         // Make sure we update to the current time
         onTimeChanged();
-
-        if (mSettingsObserver == null) {
-            mSettingsObserver = new SettingsObserver(new Handler());
-        }
-        mSettingsObserver.observe();
-        updateClockColor();
-        updateClockSize();
     }
 
     @Override
@@ -157,9 +142,6 @@ public class CustomTextClock extends TextView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (handType == 0 | handType == 1) {
-            setTextColor(mClockColor);
-        }
         if (handType == 2) {
             if (langHasChanged) {
                 setText(topText);
@@ -270,42 +252,4 @@ public class CustomTextClock extends TextView {
         return NumString;
     }
 
-    private void updateClockColor() {
-        mClockColor = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.LOCKSCREEN_CLOCK_COLOR, 0xFFFFFFFF,
-                UserHandle.USER_CURRENT);
-            setTextColor(mClockColor);
-            onTimeChanged();
-    }
-
-    public void updateClockSize() {
-        mClockSize = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.CUSTOM_TEXT_CLOCK_FONT_SIZE, 32,
-                UserHandle.USER_CURRENT);
-            setTextSize(mClockSize);
-            onTimeChanged();
-    }
-
-    protected class SettingsObserver extends ContentObserver {
-        SettingsObserver(Handler handler) {
-            super(handler);
-        }
-        void observe() {
-            ContentResolver resolver = mContext.getContentResolver();
-
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.LOCKSCREEN_CLOCK_COLOR),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.CUSTOM_TEXT_CLOCK_FONT_SIZE),
-                    false, this, UserHandle.USER_ALL);
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-	    updateClockColor();
-	    updateClockSize();
-        }
-    }
 }
-
