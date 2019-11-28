bb2e8833ac3a671d5d41ee71079c9eb113973311
Ido Ben-Hur <idoybh2@gmail.com>
2019-11-28 15:26:44 +0200

    SystemUI: Allow customizing footer text [1/2]
    Change-Id: I87d5197f37ae86808bafa71d62dae8cf5ca4f8f3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4973,6 +4973,12 @@ public final class Settings {
         public static final String QS_FOOTER_TEXT_SHOW = "qs_footer_text_show";
 
         /**
+         * QS footer text
+         * @hide
+         */
+        public static final String QS_FOOTER_TEXT_STRING = "qs_footer_text_string";
+
+        /**
 	 ** What to show in network traffic indicator in statusbar
 	 ** @hide
 	 **/
