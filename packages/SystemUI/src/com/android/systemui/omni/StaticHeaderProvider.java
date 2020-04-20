DerpLab/platform_frameworks_base_old

[1/2]base: Add qs status bar header image

packages/SystemUI/src/com/android/systemui/omni/StaticHeaderProvider.java
@@ -1,0 +1,117 @@
+/*
+ *  Copyright (C) 2017 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.systemui.omni;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
+
+import com.android.systemui.R;
+import com.android.internal.util.aosip.aosipUtils;
+
+public class StaticHeaderProvider implements
+        StatusBarHeaderMachine.IStatusBarHeaderProvider {
+
+    public static final String TAG = "StaticHeaderProvider";
+    private static final boolean DEBUG = false;
+
+    private Context mContext;
+    private Resources mRes;
+    private String mImage;
+    private String mPackageName;
+
+    public StaticHeaderProvider(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return "static";
+    }
+
+    @Override
+    public void settingsChanged(Uri uri) {
+        final boolean customHeader = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
+        String imageUrl = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER_IMAGE,
+                UserHandle.USER_CURRENT);
+
+        if (imageUrl != null && customHeader) {
+            int idx = imageUrl.indexOf("/");
+            if (idx != -1) {
+                String[] parts = imageUrl.split("/");
+                mPackageName = parts[0];
+                mImage = parts[1];
+                loadHeaderImage();
+            }
+        }
+    }
+
+    @Override
+    public void enableProvider() {
+        settingsChanged(null);
+    }
+
+    @Override
+    public void disableProvider() {
+    }
+
+    private void loadHeaderImage() {
+        if (DEBUG) Log.i(TAG, "Load header image " + " " + mPackageName + " " + mImage);
+
+        try {
+            PackageManager packageManager = mContext.getPackageManager();
+            mRes = packageManager.getResourcesForApplication(mPackageName);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to load icon pack " + mPackageName, e);
+            mRes = null;
+        }
+    }
+
+    @Override
+    public Drawable getCurrent(final Calendar now) {
+        if (mRes == null) {
+            return null;
+        }
+        if (!aosipUtils.isAvailableApp(mPackageName, mContext)) {
+            Log.w(TAG, "Header pack image " + mImage + " no longer available");
+            return null;
+        }
+        try {
+            return mRes.getDrawable(mRes.getIdentifier(mImage, "drawable", mPackageName), null);
+        } catch(Resources.NotFoundException e) {
+            Log.w(TAG, "No drawable found for " + now +" in " + mPackageName);
+        }
+        return null;
+    }
+}

packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -24,6 +24,7 @@ import android.app.AlarmManager;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.ColorStateList;
@@ -160,6 +161,29 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     }
 
     private SettingsObserver mSettingsObserver = new SettingsObserver(mHandler);
+
+    // omni additions start
+    private boolean mLandscape;
+    private boolean mHeaderImageEnabled;
+
+    private class OmniSettingsObserver extends ContentObserver {
+        OmniSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = getContext().getContentResolver();
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER), false,
+                    this, UserHandle.USER_ALL);
+            }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
+    }
+    private OmniSettingsObserver mOmniSettingsObserver = new OmniSettingsObserver(mHandler);
 
     private final BroadcastReceiver mRingerReceiver = new BroadcastReceiver() {
         @Override
@@ -325,12 +349,14 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
+        mLandscape = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE;
         updateResources();
 
         // Update color schemes in landscape to use wallpaperTextColor
         boolean shouldUseWallpaperTextColor =
                 newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE;
         mClockView.useWallpaperTextColor(shouldUseWallpaperTextColor);
+        updateStatusbarProperties();
     }
 
     @Override
@@ -348,7 +374,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 com.android.internal.R.dimen.status_bar_height);
         int qqsHeight = mContext.getResources().getDimensionPixelSize(
                 R.dimen.qs_quick_header_panel_height);
-
         setMinimumHeight(sbHeight + qqsHeight);
     }
 
@@ -361,31 +386,36 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 resources.getDimensionPixelSize(R.dimen.qs_header_tooltip_height);
         mHeaderTextContainerView.setLayoutParams(mHeaderTextContainerView.getLayoutParams());
 
-        mSystemIconsView.getLayoutParams().height = resources.getDimensionPixelSize(
-                com.android.internal.R.dimen.quick_qs_offset_height);
+        int topMargin = resources.getDimensionPixelSize(
+                com.android.internal.R.dimen.quick_qs_offset_height) + (mHeaderImageEnabled ?
+                resources.getDimensionPixelSize(R.dimen.qs_header_image_offset) : 0);
+
+        int statusBarBottomMargin = resources.getDimensionPixelSize(
+                R.dimen.qs_header_image_bottom_margin);
+
+        mSystemIconsView.getLayoutParams().height = topMargin;
         mSystemIconsView.setLayoutParams(mSystemIconsView.getLayoutParams());
 
         FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) getLayoutParams();
         if (mQsDisabled) {
-            lp.height = resources.getDimensionPixelSize(
-                    com.android.internal.R.dimen.quick_qs_offset_height);
+            lp.height = topMargin;
         } else {
-            lp.height = Math.max(getMinimumHeight(),
-                    resources.getDimensionPixelSize(
-                            com.android.internal.R.dimen.quick_qs_total_height));
+            int qsHeight = resources.getDimensionPixelSize(
+                    com.android.internal.R.dimen.quick_qs_total_height);
+
+            if (mHeaderImageEnabled) {
+                qsHeight += resources.getDimensionPixelSize(R.dimen.qs_header_image_offset);
+            }
+
+            // always add the margin below the statusbar with or without image
+            qsHeight += statusBarBottomMargin;
+            lp.height = Math.max(getMinimumHeight(), qsHeight);
         }
 
         setLayoutParams(lp);
 
         updateStatusIconAlphaAnimator();
         updateHeaderTextContainerAlphaAnimator();
-    }
-
-    private void updateSettings() {
-        updateQSBatteryMode();
-        updateSBBatteryStyle();
-        updateQSClock();
-        updateResources();
     }
 
     private void updateQSBatteryMode() {
@@ -639,5 +669,22 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             lp.leftMargin = sideMargins;
             lp.rightMargin = sideMargins;
         }
+    }
+
+    private void updateSettings() {
+        mHeaderImageEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
+        updateQSBatteryMode();
+        updateSBBatteryStyle();
+        updateQSClock();
+        updateResources();
+        updateStatusbarProperties();
+    }
+
+    // Update color schemes in landscape to use wallpaperTextColor
+    private void updateStatusbarProperties() {
+        boolean shouldUseWallpaperTextColor = mLandscape && !mHeaderImageEnabled;
+        mClockView.useWallpaperTextColor(shouldUseWallpaperTextColor);
     }
 }

derp-caf/frameworks_base

Change-Id: I10475bcfa783c5ba645749990b7374e2254b8956

packages/SystemUI/src/com/android/systemui/omni/StaticHeaderProvider.java
@@ -1,0 +1,117 @@
+/*
+ *  Copyright (C) 2017 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.systemui.omni;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
+
+import com.android.systemui.R;
+import com.android.internal.util.derp.DerpUtils;
+
+public class StaticHeaderProvider implements
+        StatusBarHeaderMachine.IStatusBarHeaderProvider {
+
+    public static final String TAG = "StaticHeaderProvider";
+    private static final boolean DEBUG = false;
+
+    private Context mContext;
+    private Resources mRes;
+    private String mImage;
+    private String mPackageName;
+
+    public StaticHeaderProvider(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return "static";
+    }
+
+    @Override
+    public void settingsChanged(Uri uri) {
+        final boolean customHeader = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
+        String imageUrl = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER_IMAGE,
+                UserHandle.USER_CURRENT);
+
+        if (imageUrl != null && customHeader) {
+            int idx = imageUrl.indexOf("/");
+            if (idx != -1) {
+                String[] parts = imageUrl.split("/");
+                mPackageName = parts[0];
+                mImage = parts[1];
+                loadHeaderImage();
+            }
+        }
+    }
+
+    @Override
+    public void enableProvider() {
+        settingsChanged(null);
+    }
+
+    @Override
+    public void disableProvider() {
+    }
+
+    private void loadHeaderImage() {
+        if (DEBUG) Log.i(TAG, "Load header image " + " " + mPackageName + " " + mImage);
+
+        try {
+            PackageManager packageManager = mContext.getPackageManager();
+            mRes = packageManager.getResourcesForApplication(mPackageName);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to load icon pack " + mPackageName, e);
+            mRes = null;
+        }
+    }
+
+    @Override
+    public Drawable getCurrent(final Calendar now) {
+        if (mRes == null) {
+            return null;
+        }
+        if (!DerpUtils.isAvailableApp(mPackageName, mContext)) {
+            Log.w(TAG, "Header pack image " + mImage + " no longer available");
+            return null;
+        }
+        try {
+            return mRes.getDrawable(mRes.getIdentifier(mImage, "drawable", mPackageName), null);
+        } catch(Resources.NotFoundException e) {
+            Log.w(TAG, "No drawable found for " + now +" in " + mPackageName);
+        }
+        return null;
+    }
+}

packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -23,6 +23,7 @@ import android.app.ActivityManager;
 import android.app.AlarmManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.ColorStateList;
@@ -164,6 +165,9 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     private int mSystemInfoMode;
     private ImageView mSystemInfoIcon;
     private View mSystemInfoLayout;
+    // omni additions start
+    private boolean mLandscape;
+    private boolean mHeaderImageEnabled;
 
     protected ContentResolver mContentResolver;
 
@@ -177,6 +181,9 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.QS_SYSTEM_INFO), false,
                     this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER), false,
+                    this, UserHandle.USER_ALL);
             }
 
         @Override
@@ -184,7 +191,8 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             updateSettings();
         }
     }
-    private SettingsObserver mSettingsObserver = new SettingsObserver(mHandler); 
+    private SettingsObserver mSettingsObserver = new SettingsObserver(mHandler);
+
     private final BroadcastReceiver mRingerReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -470,12 +478,14 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
+        mLandscape = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE;
         updateResources();
 
         // Update color schemes in landscape to use wallpaperTextColor
         boolean shouldUseWallpaperTextColor =
                 newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE;
         mClockView.useWallpaperTextColor(shouldUseWallpaperTextColor);
+        updateStatusbarProperties();
     }
 
     @Override
@@ -493,7 +503,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 com.android.internal.R.dimen.status_bar_height);
         int qqsHeight = mContext.getResources().getDimensionPixelSize(
                 R.dimen.qs_quick_header_panel_height);
-
         setMinimumHeight(sbHeight + qqsHeight);
     }
 
@@ -506,18 +515,30 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 resources.getDimensionPixelSize(R.dimen.qs_header_tooltip_height);
         mHeaderTextContainerView.setLayoutParams(mHeaderTextContainerView.getLayoutParams());
 
-        mSystemIconsView.getLayoutParams().height = resources.getDimensionPixelSize(
-                com.android.internal.R.dimen.quick_qs_offset_height);
+        int topMargin = resources.getDimensionPixelSize(
+                com.android.internal.R.dimen.quick_qs_offset_height) + (mHeaderImageEnabled ?
+                resources.getDimensionPixelSize(R.dimen.qs_header_image_offset) : 0);
+
+        int statusBarBottomMargin = resources.getDimensionPixelSize(
+                R.dimen.qs_header_image_bottom_margin);
+
+        mSystemIconsView.getLayoutParams().height = topMargin;
         mSystemIconsView.setLayoutParams(mSystemIconsView.getLayoutParams());
 
         FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) getLayoutParams();
         if (mQsDisabled) {
-            lp.height = resources.getDimensionPixelSize(
-                    com.android.internal.R.dimen.quick_qs_offset_height);
+            lp.height = topMargin;
         } else {
-            lp.height = Math.max(getMinimumHeight(),
-                    resources.getDimensionPixelSize(
-                            com.android.internal.R.dimen.quick_qs_total_height));
+            int qsHeight = resources.getDimensionPixelSize(
+                    com.android.internal.R.dimen.quick_qs_total_height);
+
+            if (mHeaderImageEnabled) {
+                qsHeight += resources.getDimensionPixelSize(R.dimen.qs_header_image_offset);
+            }
+
+            // always add the margin below the statusbar with or without image
+            qsHeight += statusBarBottomMargin;
+            lp.height = Math.max(getMinimumHeight(), qsHeight);
         }
 
         setLayoutParams(lp);
@@ -527,10 +548,14 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         updatePrivacyChipAlphaAnimator();
     }
 
-      private void updateSettings() {
+    private void updateSettings() {
+        mHeaderImageEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
         mSystemInfoMode = getQsSystemInfoMode();
         updateSystemInfoText();
         updateResources();
+        updateStatusbarProperties();
      }
 
     private void updateStatusIconAlphaAnimator() {
@@ -785,5 +810,11 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             lp.leftMargin = sideMargins;
             lp.rightMargin = sideMargins;
         }
+    }
+
+    // Update color schemes in landscape to use wallpaperTextColor
+    private void updateStatusbarProperties() {
+        boolean shouldUseWallpaperTextColor = mLandscape && !mHeaderImageEnabled;
+        mClockView.useWallpaperTextColor(shouldUseWallpaperTextColor);
     }
 }