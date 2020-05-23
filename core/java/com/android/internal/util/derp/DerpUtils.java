771901e745f44a95d5e60d7bceed63455eedf1c4
AnkitGourav <Ankitgourav035@gmail.com>
2020-05-23 09:14:37 +0530

    DerpUtils: add method to get today's passed time

--- core/java/com/android/internal/util/derp/DerpUtils.java
+++ core/java/com/android/internal/util/derp/DerpUtils.java
@@ -32,6 +32,7 @@ import android.os.SystemClock;
 import android.net.NetworkInfo;
 import android.net.ConnectivityManager;
 import android.os.SystemProperties;
+import android.text.format.Time;
 import android.os.UserHandle;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
@@ -69,6 +70,15 @@ public class DerpUtils {
 
         NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         return wifi.isConnected();
+    }
+
+	// Returns today's passed time in Millisecond
+    public static long getTodayMillis() {
+        final long passedMillis;
+        Time time = new Time();
+        time.set(System.currentTimeMillis());
+        passedMillis = ((time.hour * 60 * 60) + (time.minute * 60) + time.second) * 1000;
+        return passedMillis;
     }
 
 	// Check if device is connected to the internet
