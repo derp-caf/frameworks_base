444540d132940da10ca1d9e653fdf27c86515393
Ido Ben-Hur <idoybh2@gmail.com>
2020-10-11 04:29:36 +0300

    base: Allow hiding auto brightness toggle [1/2]
    Change-Id: I8abcf0a2c9dc25fd70702352ad9cccc405d53920

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4991,6 +4991,12 @@ public final class Settings {
         public static final String QS_BRIGHTNESS_POSITION_BOTTOM = "qs_brightness_position_bottom";
 
         /**
+         * Whether to show the brighness slider auto brightness toggle
+         * @hide
+         */
+        public static final String QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
+
+        /**
 	 ** What to show in network traffic indicator in statusbar
 	 ** @hide
 	 **/
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -93,5 +93,6 @@ public class SystemSettings {
         Settings.System.QS_FOOTER_TEXT_STRING,
         Settings.System.QS_SHOW_BRIGHTNESS,
         Settings.System.QS_BRIGHTNESS_POSITION_BOTTOM,
+        Settings.System.QS_SHOW_AUTO_BRIGHTNESS,
     };
 }
